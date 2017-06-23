91a0100e3a3cfe09c6bc6dbef5766933ad506ce3
BrycePearce <bpearce@angelo.edu>
2017-06-22 22:43:53 -0500

    added searchBox component / project structure

---
+++ src/app/components/searchbox.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+@Component({
+  //how we select this component
+  selector: 'search-box',
+  //our html for this component
+  templateUrl: './searchbox.component.html'
+
+})
+export class SearchBox {
+  name = '';
+}
